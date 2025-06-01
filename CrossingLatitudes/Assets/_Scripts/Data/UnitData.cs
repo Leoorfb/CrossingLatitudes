774a0675be8b3e46b01619675349f936424cabef
Ramon Souza <ramonsouzasil2004@gmail.com>
2025-06-01 11:22:55 -0300

    Subindo enemeies scriptablesobjt

--- CrossingLatitudes/Assets/_Scripts/Data/UnitData.cs
+++ CrossingLatitudes/Assets/_Scripts/Data/UnitData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UnitData : ScriptableObject
 {
     [field: SerializeField] public Sprite Image { get; private set; }
+    [field: SerializeField] public GameObject Prefab { get; private set; }
     [field: SerializeField] public int MaxHealth { get; private set; }
     [field: SerializeField] public int AttackPower { get; private set; }
     [field: SerializeField] public int DefensePower { get; private set; }
