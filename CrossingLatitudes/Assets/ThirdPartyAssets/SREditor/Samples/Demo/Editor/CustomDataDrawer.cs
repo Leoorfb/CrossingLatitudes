5a67d5ebcd91482152fabad653f617762e75f419
Ramon Souza <ramonsouzasil2004@gmail.com>
2025-06-01 11:49:47 -0300

    Editor resolve v2

--- CrossingLatitudes/Assets/ThirdPartyAssets/SREditor/Samples/Demo/Editor/CustomDataDrawer.cs
+++ CrossingLatitudes/Assets/ThirdPartyAssets/SREditor/Samples/Demo/Editor/CustomDataDrawer.cs
@@ -1,3 +1,5 @@
+#if UNITY_EDITOR
+
 using Demo;
 using SerializeReferenceEditor.Editor;
 using UnityEditor;
@@ -5,23 +7,23 @@ using UnityEngine;
 
 namespace Demo.Editor
 {
-	#if UNITY_EDITOR
-	[CustomPropertyDrawer(typeof(CustomData), false)]
-	public class CustomDataDrawer : PropertyDrawer
-	{
-		private SRDrawer _drawer = new();
-		
-		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-		{
-			var dataProperty = property.FindPropertyRelative("Data");
-			_drawer.Draw(position, dataProperty, new GUIContent("Custom Title"));
-		}
+    [CustomPropertyDrawer(typeof(CustomData), false)]
+    public class CustomDataDrawer : PropertyDrawer
+    {
+        private SRDrawer _drawer = new();
 
-		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-		{
-			var dataProperty = property.FindPropertyRelative("Data");
-			return _drawer.GetPropertyHeight(dataProperty, label);
-		}
-	}
-	#endif
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var dataProperty = property.FindPropertyRelative("Data");
+            _drawer.Draw(position, dataProperty, new GUIContent("Custom Title"));
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var dataProperty = property.FindPropertyRelative("Data");
+            return _drawer.GetPropertyHeight(dataProperty, label);
+        }
+    }
 }
+
+#endif
